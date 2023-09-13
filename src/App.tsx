61758d54af7b4d2f35147be7e6eddecb51b7fcdd
nsx_69 <mrmanafest16@gmail.com>
2023-09-14 02:29:02 +0500

    Changed site direction

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-        {/*HEADER IS STARTED*/}
-      <header className="App-header">
-
-      </header>
-        {/*HEADER IS FINISHED*/}
-
-        {/*MAIN IS STARTED*/}
-        <main className="App-main">
-
-        </main>
-        {/*MAIN IS FINISHED*/}
-
-        {/*FOOTER IS STARTED*/}
-        <footer className="App-footer">
-
-        </footer>
-        {/*FOOTER IS FINISHED*/}
-    </div>
+    <header className="App-header">
+        <h1 className="App-title">
+            The End.
+        </h1>
+    </header>
   );
 }
 
