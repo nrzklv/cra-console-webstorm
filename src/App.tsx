7adfdbcb613e9c2525e2fa857037c980252e32ad
nsx_69 <mrmanafest16@gmail.com>
2023-09-13 18:17:26 +0500

    added the desired view

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+        {/*HEADER IS STARTED*/}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          My first Project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+        {/*HEADER IS FINISHED*/}
+
+        {/*MAIN IS STARTED*/}
+        <main className="App-main">
+
+        </main>
+        {/*MAIN IS FINISHED*/}
+
+        {/*FOOTER IS STARTED*/}
+        <footer className="App-footer">
+
+        </footer>
+        {/*FOOTER IS FINISHED*/}
     </div>
   );
 }
