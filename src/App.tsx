aec293e005e3d25a68290e7c5cb8241b39e82626
nsx_69 <mrmanafest16@gmail.com>
2023-09-13 15:48:25 +0500

    add new line of text

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          My first Project
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
